ec23f49d98470ed5d48137f120003d888cda99a9
IamLAM <ingeniero@luismonge.com.mx>
2018-05-13 06:15:57 +0100

    Se detecto longitud tweet y clasificaci¢n por retweet y favoritos

--- aeTwitter.R
+++ aeTwitter.R
@@ -10,3 +10,22 @@ getCurRateLimitInfo(c('users','followers','account'))
 #obtener tweets
 tdf<-twListToDF(searchTwitter("Merca20",n=100,lang="es"))
 names(tdf)
+summary(tdf)
+unique(tdf$screenName)
+#contabilizaremos el numero de caracteres enviados
+numC<-nchar(tdf$text)
+
+#añadiremos al dataframe la columna de numero de caracteres
+
+tdf<-data.frame(tdf,caracteres=numC)
+
+#clasificaremos los tweets de acuerdo al numero de retweets y favoritos
+
+tdf$tipo[tdf$isRetweet&tdf$favoriteCount>0]<-'A'
+tdf$tipo[tdf$isRetweet&tdf$favoriteCount==0]<-'B'
+tdf$tipo[!tdf$isRetweet&tdf$favoriteCount>0]<-'C'
+tdf$tipo[!tdf$isRetweet&tdf$favoriteCount==0]<-'D'
+
+tdf$tipo<-as.factor(tdf$tipo)
+
+
